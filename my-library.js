bfc8a43bddc9aeb0c9b8341e3cd5c000439fa6e4
Lucy <lucynn11@gmail.com>
2019-01-26 18:55:51 +0400

    edits to package.json

--- my-library.js
+++ my-library.js
@@ -2,6 +2,8 @@
 const uuid = require('uuid');
 const newUuid = uuid();
 
+console.log(`youe new Uniersally unique identifier is ${newUuid}`);
+
 module.exports = {
     myFirstMethod: () => {
         console.log('This is my first method');
